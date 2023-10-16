17f29402763e23a1f8a46d4f95b096eef2faed16
tomDevEdge <148066546+tomDevEdge@users.noreply.github.com>
2023-10-16 15:35:42 +0100

    neaten up testing, provide an edge case

--- fibonacci.js
+++ fibonacci.js
@@ -14,15 +14,14 @@ function nthFibonacci(n) {
   console.log(`The Fibonacci number at position ${n} is ${fib[n]}`);
 }
 
-// Call the function with the desired Fibonacci number position
+// Testing
 nthFibonacci(0); // Expected output: 0
-// Test for n = 1:
 nthFibonacci(1); // Expected output: 1
-// Test for n = 2:
 nthFibonacci(2); // Expected output: 1
-// Test for n = 5:
 nthFibonacci(5); // Expected output: 5
-// Test for n = 10:
 nthFibonacci(10); // Expected output: 55
-// Test for n = 20:
 nthFibonacci(20); // Expected output: 6765
+
+
+// Edge cases
+(nthFibonacci(100)); // Expected output: 782204094 (large Fibonacci number)
