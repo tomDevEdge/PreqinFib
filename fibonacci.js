06687ab9c7b2f536b69815488d86de89fc95f9f0
tomDevEdge <148066546+tomDevEdge@users.noreply.github.com>
2023-10-16 15:26:45 +0100

    log the output instead of displaying in the console

--- fibonacci.js
+++ fibonacci.js
@@ -10,6 +10,20 @@ function nthFibonacci(n) {
     fib[i] = (fib[i - 1] + fib[i - 2]) % 1000000007;
   }
 
-  // Return the nth Fibonacci number modulo 1000000007
-  return fib[n];
+  // Log the nth Fibonacci number modulo 1000000007
+  console.log(`The Fibonacci number at position ${n} is ${fib[n]}`);
 }
+// Call the function with the desired Fibonacci number position
+(nthFibonacci(0)); // Expected output: 0
+//Test for n = 1:
+(nthFibonacci(1)); // Expected output: 1
+//Test for n = 2:
+(nthFibonacci(2)); // Expected output: 1
+//Test for n = 5:
+(nthFibonacci(5)); // Expected output: 5
+//Test for n = 10:
+(nthFibonacci(10)); // Expected output: 55
+//Test for n = 20:
+(nthFibonacci(20)); // Expected output: 6765
+
+
